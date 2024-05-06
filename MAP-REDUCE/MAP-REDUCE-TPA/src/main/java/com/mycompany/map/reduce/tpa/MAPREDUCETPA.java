2f7dbfcaa876f4b8aa04531f9615220f9047786c
Nick Mathieu <rakotomanananick29@gmail.com>
2024-05-06 21:29:14 +0300

    [MAP-REDUCE] Projet Init

---
+++ MAP-REDUCE/MAP-REDUCE-TPA/src/main/java/com/mycompany/map/reduce/tpa/MAPREDUCETPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.map.reduce.tpa;
+
+/**
+ *
+ * @author rakot
+ */
+public class MAPREDUCETPA {
+
+    public static void main(String[] args) {
+        System.out.println("Jointure entre CO2 et Catalogues");
+    }
+}
