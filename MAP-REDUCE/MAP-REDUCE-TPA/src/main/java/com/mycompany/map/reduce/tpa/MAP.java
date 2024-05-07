eb1bdae2153a5a1f244bb87065fcd3971e0498af
Nick Mathieu <rakotomanananick29@gmail.com>
2024-05-07 21:47:16 +0300

    [TPA] Création du Mapper

--- MAP-REDUCE/MAP-REDUCE-TPA/src/main/java/com/mycompany/map/reduce/tpa/MAP.java
+++ MAP-REDUCE/MAP-REDUCE-TPA/src/main/java/com/mycompany/map/reduce/tpa/MAP.java
@@ -4,10 +4,62 @@
  */
 package com.mycompany.map.reduce.tpa;
 
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
 /**
  *
  * @author rakot
  */
-public class MAP {
+public class MAP extends Mapper<LongWritable, Text, Text, Text> {
+
+    private Text value = new Text();
+    private Text marque = new Text();
+    private double sommeCO2 = 0;
+    private double sommeBonusMalus = 0;
+    private double sommeCout = 0;
+    private int indice = 0;
+
+    @Override
+    protected void map(LongWritable cle, Text valeur, Context context) throws IOException, InterruptedException {
+        Configuration conf = context.getConfiguration();
+        if (cle.get() > 0) {
+            String[] data = valeur.toString().split(",");
+            if (valeur.toString().contains("€")) {
+                marque.set(data[0].toUpperCase());
+                value.set("isCataloque;/" + data[1].toUpperCase() + ";/" + data[2] + ";/" + data[3] + ";/" + data[4] + ";/" + data[5] + ";/" + data[6] + ";/" + data[7] + ";/" + data[8]);
+            } else {
+                marque.set(data[1].split(" ")[0]);
+                String bonusMalus = clearBonusMalus(data[2]);
+                String co2 = data[3].replaceAll("[^\\d.-]", "");
+                String coutEnergie = data[4].replaceAll("[^\\d.-]", "");
+                value.set("isCo2;/" + data[1].split(" ")[1].toUpperCase() + ";/" + bonusMalus + ";/" + co2 + ";/" + coutEnergie);
+                this.indice++;
+                this.sommeCO2= Double.parseDouble(co2);
+                this.sommeCout += Double.parseDouble(coutEnergie);
+                double valBonusMalus = Double.parseDouble(bonusMalus);
+                this.sommeBonusMalus += valBonusMalus;
+            }
+            context.write(marque, value);
+        }
+    }
+
+    public static String clearBonusMalus(String val) {
+        String noNumerique=val.replaceAll("[^\\d.-]", "");
+        if (noNumerique.equals("") && noNumerique.equals("-")) {
+            return "0";
+        }
+        val = val.split("€")[0];
+        val = val.replaceAll("\\s+", "");
+        return val;
+    }
     
+    @Override
+    public void cleanup(Context context) throws IOException, InterruptedException {
+        String rep = "Total;/" + this.sommeCO2 + ";/" + this.sommeCout + ";/" + this.sommeBonusMalus + ";/" + this.indice;
+        context.write(new Text("TOTAL"), new Text(rep));
+    }
 }
