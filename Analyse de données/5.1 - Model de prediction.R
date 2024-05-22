459a9267072c69f7d34eb2eb0650c7213b7ba380
Loïc <lorandrianarivelo@gmail.com>
2024-05-22 23:47:58 +0300

    FIx

--- Analyse de données/5.1 - Model de prediction.R
+++ Analyse de données/5.1 - Model de prediction.R
@@ -1,10 +1,12 @@
 install.packages("rpart")
 install.packages("C50")
 install.packages("rpart.plot")
+install.packages("partykit")
 library(rpart)
 library(rpart.plot)
 library(C50)
 library(tree)
+library(partykit)
 
 #Suppression variables inutiles pour prédiction Marketing
 clients_immatriculations <- clients_immatriculations[ , !(names(clients_immatriculations) %in% "immatriculation")]
@@ -23,7 +25,6 @@ clients_immatriculations <- clients_immatriculations[ , !(names(clients_immatric
 str(clients_immatriculations)
 
 #Modification des données pour conformer aux normes imposées par les modèles
-clients_immatriculations[clients_immatriculations$categorie == 'Coupé', c('categorie')] <- 'Coupe'
 clients_immatriculations[clients_immatriculations$situationfamiliale == 'Célibataire', c('situationfamiliale')] <- 'Celibataire'
 
 clients_immatriculations$sexe <- as.factor(clients_immatriculations$sexe)
@@ -36,9 +37,9 @@ str(clients_immatriculations)
 
 # Création des données d'apprentissage et des données de test
 nrow(clients_immatriculations)
-#197843 || 80/20 || Test: 39569 et test 158275
-test_data <- clients_immatriculations[1:39569, ]
-training_data <- clients_immatriculations[39570:158275, ]
+#196677 || 80/20 || Test: 39336 et apprentissage 157341
+test_data <- clients_immatriculations[1:39336, ]
+training_data <- clients_immatriculations[39337:196677, ]
 
 
 
@@ -73,14 +74,15 @@ taux_rpartinfo
 
 # Model à partir de C5.0
 c5 <- C5.0(categorie~., training_data)
-# plot(c5)
+c5_party <- as.party(c5)
+plot(c5_party, type = "simple", gp = gpar(fontsize = 10))
 test_tree2 <- predict(c5, test_data, type="class")
 test_data$Tree2 <- test_tree2
 taux_succes2 <- nrow(test_data[test_data$categorie==test_data$Tree2,])/nrow(test_data)
 taux_succes2
 
 # Model à partir de tree
-tree3 <- tree(categorie~., training_data)
+treeAuto <- tree(categorie~., training_data)
 
 tree_tr1 <- tree(categorie~., training_data, split = "deviance", control = tree.control(nrow(training_data)))
 plot(tree_tr1)
@@ -91,11 +93,15 @@ plot(tree_tr3)
 text(tree_tr3, pretty = 0)
 
 ##################################
-test_tree3 <- predict(tree3, test_data, type="class")
-test_data$Tree3 <- test_tree3
-taux_succes3 <- nrow(test_data[test_data$categorie==test_data$Tree3,])/nrow(test_data)
-taux_succes3
+test_treeAuto <- predict(treeAuto, test_data, type="class")
+test_data$TreeAuto <- test_treeAuto
+taux_succesAuto <- nrow(test_data[test_data$categorie==test_data$TreeAuto,])/nrow(test_data)
+taux_succesAuto
 
 test_tree_tr1 <- predict(tree_tr1, test_data, type="class")
 taux_succes_tr2 <- nrow(test_data[test_data$categorie==test_tree_tr1,])/nrow(test_data)
 taux_succes_tr2
+
+test_tree_3 <- predict(tree_tr3, test_data, type="class")
+taux_succes_tr3 <- nrow(test_data[test_data$categorie==test_tree_3,])/nrow(test_data)
+taux_succes_tr3
