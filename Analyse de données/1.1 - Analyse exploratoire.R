8eec632cac896eabbdd119721a704496a2710103
Loïc <lorandrianarivelo@gmail.com>
2024-05-23 00:13:36 +0300

    Fix

--- Analyse de données/1.1 - Analyse exploratoire.R
+++ Analyse de données/1.1 - Analyse exploratoire.R
@@ -5,25 +5,15 @@ check_duplicate <- function(data) {
   print(duplicate_data)
 }
 
-# Fonction pour créer un histogramme des effectifs pour une variable
-create_histogram <- function(data, var_name) {
-  hist(data[[var_name]], main = paste("Histogramme de", var_name), xlab = var_name, col = "skyblue", border = "white")
-}
-
 # Fonction pour effectuer une analyse exploratoire complète
 explore_data <- function(data) {
   # Histogrammes pour les variables numériques
   numeric_vars <- sapply(data, is.numeric)
   numeric_data <- data[, numeric_vars]
-  # print(numeric_data)
-  # apply(numeric_data, 2, function(x) create_histogram(data, names(data)[which(names(data) == names(x))]))
-  # 
-  # # Nuages de points pour les paires de variables numériques
-  # pairs(numeric_data)
-  # 
-  # # Boîtes à moustaches pour les variables numériques
+  
+  # Boîtes à moustaches pour les variables numériques
   boxplot(numeric_data)
-  # 
+   
   # # Tableaux de fréquence pour les variables catégorielles
   categorical_vars <- sapply(data, function(x) is.factor(x) || is.character(x))
   categorical_data <- data[, categorical_vars]
